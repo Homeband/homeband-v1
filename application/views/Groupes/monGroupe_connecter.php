0b3a6ffb2cddb76a83033252c9817577e29b4448
christopher <33717563+Saovers@users.noreply.github.com>
2017-12-05 18:54:22 +0100

    mon groupe

--- application/views/Groupes/monGroupe_connecter.php
+++ application/views/Groupes/monGroupe_connecter.php
@@ -1,2 +1,87 @@
 <h1> Mon Groupe</h1>
+<h3> Vos liens</h3>
+<div class="wrap">
+    <div class="card">
+        <div class="card-body my-card-body">
+            <?php echo form_open('', array('id'=> 'formInscription','class' => 'col-sm-10 col-md-8 col-lg-7 col-centered')); ?>
 
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Facebook</label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input id="text" type="text" name="evenements" class="form-control" value="link/facebook">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Ajouter/modifier le lien </label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input type="text" name="salle" class="form-control"  value="" size="50"/>
+                </div>
+            </div>
+            <br/>
+            <div class="form-group row">
+                <div class="col-sm-12 col-md-12 text-center" >
+                    <input type="submit" value="Modifier" class="btn btn-homeband"/>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="wrap">
+    <div class="card">
+        <div class="card-body my-card-body">
+            <?php echo form_open('', array('id'=> 'formInscription','class' => 'col-sm-10 col-md-8 col-lg-7 col-centered')); ?>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Instagram</label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input id="text" type="text" name="evenements" class="form-control" value="link/insta">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Ajouter/modifier le lien </label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input type="text" name="salle" class="form-control"  value="" size="50"/>
+                </div>
+            </div>
+            <br/>
+            <div class="form-group row">
+                <div class="col-sm-12 col-md-12 text-center" >
+                    <input type="submit" value="Modifier" class="btn btn-homeband"/>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="wrap">
+    <div class="card">
+        <div class="card-body my-card-body">
+            <?php echo form_open('', array('id'=> 'formInscription','class' => 'col-sm-10 col-md-8 col-lg-7 col-centered')); ?>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Twitter</label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input id="text" type="text" name="evenements" class="form-control" value="link/twitter">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Ajouter/modifier le lien </label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input type="text" name="salle" class="form-control"  value="" size="50"/>
+                </div>
+            </div>
+            <br/>
+            <div class="form-group row">
+                <div class="col-sm-12 col-md-12 text-center" >
+                    <input type="submit" value="Modifier" class="btn btn-homeband"/>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+<h3>Albums</h3>
--- application/views/Groupes/profil_connecter.php
+++ application/views/Groupes/profil_connecter.php
@@ -1,1 +1,66 @@
 <h1> Profil </h1>
+<h3> Votre email</h3>
+<div class="wrap">
+    <div class="card">
+        <div class="card-body my-card-body">
+            <?php echo form_open('', array('id'=> 'formInscription','class' => 'col-sm-10 col-md-8 col-lg-7 col-centered')); ?>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Votre mail actuel</label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input id="text" type="text" name="evenements" class="form-control" value="test@hotmail.fr">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Nouvel email </label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input type="text" name="salle" class="form-control"  value="" size="50"/>
+                </div>
+            </div>
+            <br/>
+
+            <div class="form-group row">
+                <div class="col-sm-12 col-md-12 text-center" >
+                    <input type="submit" value="Modifier" class="btn btn-homeband"/>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+<h3> Changer votre mot de passe </h3>
+<div class="wrap">
+    <div class="card">
+        <div class="card-body my-card-body">
+            <?php echo form_open('', array('id'=> 'formInscription','class' => 'col-sm-10 col-md-8 col-lg-7 col-centered')); ?>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Ancien mot de passe</label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input id="password" type="text" name="evenements" class="form-control" value="">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Nouveau mot de passe </label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input type="password" name="salle" class="form-control"  value="" size="50"/>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="username" class="col-md-5 col-lg-4 col-form-label">Réécrire le mot de passe</label>
+                <div class="col-sm-12 col-md-7 col-lg-8">
+                    <input type="password" name="date" class="form-control" value="" size="50"/>
+                </div>
+            </div>
+            <br/>
+            <div class="form-group row">
+                <div class="col-sm-12 col-md-12 text-center" >
+                    <input type="submit" value="Modifier" class="btn btn-homeband"/>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
