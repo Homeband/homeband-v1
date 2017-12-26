c77452354f9744631c67dd48c141804c7246aaec
Nicolas Gérard <nicolas.gerard@proximus.be>
2017-12-26 22:11:03 +0100

    Modification JS inscriptions

--- assets/js/inscription.js
+++ assets/js/inscription.js
@@ -1,51 +1,60 @@
 $(document).ready(function () {
 
-    $("#code_postal").keyup(function(){
-        cp =  $("#code_postal").val();
-        if(cp.length < 4){
-            $('#villes').empty();
-        } else {
+    var selVilles = "#villes";
+    var selCP = "#code_postal";
+    var selCU = "#checkCU";
+    var selForm = "#formInscription";
+
+    updateVilles();
+
+    $(selCP).keyup(function(){
+
             updateVilles();
-        }
     });
 
-    function updateVilles() {
-        var id_select = "#villes";
-        var id_cp = "#code_postal";
-
-        var cp = $(id_cp).val();
-        var url = api + 'villes';
-        var params = {'cp' : cp};
-
-        // Appel de la fonction jQuery (Ajax)
-        $.get(url, params, function(data){
-            // 1 - Vider le champs SELECT
-            $(id_select).empty();
-
-            // 2 - Remplissage avec les nouvelles valeurs
-            for(var key in data.liste){
-                var ville = data.liste[key];
-
-                var nom = ville.nom;
-                var id_villes = ville.id_villes;
-
-                $(id_select).append('<option value="' + id_villes + '">' + nom + '</option>');
-            }
-        }, 'json');
-
-    }
-
-    $("#formInscription").submit(function(e){
-        var statutCu = $("#checkCU").prop("checked");
+    $(selForm).submit(function(e){
+        var statutCu = $(selCU).prop("checked");
         if(statutCu==false){
             alert("Vous devez accepter les conditions d'utilisations");
             return false;
         }
-
-
-    })
+    });
 
     $("#MaTable").DataTable();
+
+    // Fonctions
+
+    function updateVilles() {
+        var cp =  $(selCP).val();
+        if(cp.length < 4){
+            $(selVilles).empty();
+            $(selVilles).append('<option value="0">Entrez un code postal</option>')
+            $(selVilles).prop("disabled", true);
+        } else {
+
+            var cp = $(selCP).val();
+            var url = api + 'villes';
+            var params = {'cp' : cp};
+
+            // Appel de la fonction jQuery (Ajax)
+            $.get(url, params, function(data){
+                // 1 - Vider le champs SELECT
+                $(selVilles).empty();
+                $(selVilles).prop("disabled", false);
+
+                // 2 - Remplissage avec les nouvelles valeurs
+                for(var key in data.liste){
+                    var ville = data.liste[key];
+
+                    var nom = ville.nom;
+                    var id_villes = ville.id_villes;
+
+                    $(selVilles).append('<option value="' + id_villes + '">' + nom + '</option>');
+                }
+            }, 'json');
+        }
+    }
+
 });
 
 
