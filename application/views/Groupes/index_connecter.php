2ac2b6685f1d04a17c8fc0705027b94ac92fb66c
Nicolas Gérard <nicolas.gerard@proximus.be>
2017-12-30 22:46:20 +0100

    Résolution d'erreur

--- application/views/Groupes/index_connecter.php
+++ application/views/Groupes/index_connecter.php
@@ -1,3 +1,8 @@
+<?php
+    if(!isset($groupe)){
+        $groupe = new Groupe();
+    }
+?>
 <h1> Bienvenue <?= $groupe->nom ?></h1>
 <p>Ceci est votre page de gestion, vous pourrez changer vos informations, ajouter des newsletter, ajouter des évènements et voir vos commentaires</p>
 
