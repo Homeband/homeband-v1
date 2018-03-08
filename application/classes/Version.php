122e7a4558e464eb45595ecd8342e9dbfeb1d09e
Nicolas <nicolas.gerard@std.heh.be>
2018-03-08 16:41:03 +0100

    Ajout de la classe pour la gestion des versions des tables de référence

---
+++ application/classes/Version.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrateur
+ * Date: 08-03-18
+ * Time: 15:05
+ */
+
+class Version extends MY_Object
+{
+    public $id_versions = 0;
+    public $num_table = 0;
+    public $nom_table = '';
+    public $date_maj = '';
+}
