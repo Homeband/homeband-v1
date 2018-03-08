dba110050ad458165ba22150947b39bd0e79a3f0
Nicolas <nicolas.gerard@std.heh.be>
2018-03-08 16:42:12 +0100

    Ajout des nouveaux champs pour le groupe
    date_maj : date de la dernière mise à jour
    api_ck : Customer Key (pour les appels à l'API)

--- application/classes/Groupe.php
+++ application/classes/Groupe.php
@@ -17,9 +17,11 @@ class Groupe extends MY_Object
     public $lien_spotify = '';
     public $lien_twitter = '';
     public $lien_youtube = '';
+    public $date_maj = '';
     public $est_actif = true;
     public $id_styles = 0;
     public $id_villes = 0;
+    public $api_ck = '';
 
     public function hash_password(){
         $this->mot_de_passe = password_hash($this->mot_de_passe, PASSWORD_DEFAULT);
